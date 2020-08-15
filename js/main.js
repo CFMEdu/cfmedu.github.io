cda5dcf2f2b955203567912cd2fd157434afbbfd
CFMEdu <69703969+CFMEdu@users.noreply.github.com>
2020-08-14 19:26:41 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,73 @@
+// --------------------------------------------------
+// Navigation Toggle
+// --------------------------------------------------
+$(function() {
+  $(".navToggle").click(function() {
+    $(".navToggle").toggleClass("active");
+    $(".navMenu").toggleClass("active");
+  })
+  $(".navMenu a").click(function() {
+    $(".navToggle").removeClass("active");
+    $(".navMenu").removeClass("active");
+  });
+});
+
+// --------------------------------------------------
+// Hide Menu on Desktop View Only Scroll
+// --------------------------------------------------
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  var windowWidth = $(window).width();
+  if (windowWidth > 767) {
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navBar").style.top = "0";
+    } else {
+      document.getElementById("navBar").style.top = "-50px";
+    }
+  } else {
+    document.getElementById("navBar").style.top = "0";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navBar").style.top = "0";
+//   } else {
+//     document.getElementById("navBar").style.top = "-50px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
+
+// --------------------------------------------------
+// Disable skrollr on Movile View
+// --------------------------------------------------
+$(function() {
+  var s = skrollr.init({
+    forceHeight: false
+  });
+  if (s.isMobile()) {
+      s.destroy();
+  }
+});
+
+// --------------------------------------------------
+// External Link Warning
+// --------------------------------------------------
+$("a").filter(function() {
+  return this.hostname && this.hostname !== location.hostname;
+}).click(function(e) {
+  if(!confirm("You are about to proceed to an external website."))
+    {
+      e.preventDefault();
+    };
+});
+
+
+
+
+
+
