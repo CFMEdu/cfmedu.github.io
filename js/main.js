1e9381e62ec43843158d7d6c79d7f15fdea76456
CFMEdu <69703969+CFMEdu@users.noreply.github.com>
2020-09-03 16:45:12 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -25,63 +25,69 @@ $(function() {
 // --------------------------------------------------
 // Shrink Nav Bar on Scroll
 // --------------------------------------------------
-$(function() {  
-  $(window).scroll(function(){
-    var position = $(this).scrollTop();
-    if(position > 50) {
-      $(".navBar, .navLogo a, .navLogo img, .navMenu > ul > li > a, .iconBar, .navDeco, .navDropdown").addClass("shrink");
-    } else {
-      $(".navBar, .navLogo a, .navLogo img, .navMenu > ul > li > a, .iconBar, .navDeco, .navDropdown").removeClass("shrink");
-    }
-  });
+$(window).scroll(function(){
+  var position = $(this).scrollTop();
+  if(position > 50) {
+    $(".navBar, .navLogo a, .navLogo img, .navMenu > ul > li > a, .iconBar, .navDeco, .navDropdown").addClass("shrink");
+  } else {
+    $(".navBar, .navLogo a, .navLogo img, .navMenu > ul > li > a, .iconBar, .navDeco, .navDropdown").removeClass("shrink");
+  }
 });
 
 
 // --------------------------------------------------
-// Sticky Subnavigation Menu
+// Sticky Subnavigation Menu on Scroll
 // --------------------------------------------------
+$(window).scroll(function() {
+  var windowTop = $(window).scrollTop() + 50; 
+  var divTop = $("#checkDiv").offset().top;
+  if (windowTop >= divTop) {
+    $(".subNavigation").addClass("stickyDiv");
+  } else {
+    $(".subNavigation").removeClass("stickyDiv");
+  }
+});
 
+
+
+// --------------------------------------------------
+// Smooth Scrolling on Click
+// --------------------------------------------------
 $(function() {
   $(document).on("scroll", onScroll);
-  
-  // add sticky class to subnavigation on scroll
-  $(window).scroll(function() {
-    var windowTop = $(window).scrollTop() + 50; 
-    var divTop = $("#checkDiv").offset().top;
-    if (windowTop >= divTop) {
-      $(".subNavigation").addClass("stickyDiv");
-    } else {
-      $(".subNavigation").removeClass("stickyDiv");
+
+  $("a").on('click', function(e) {
+    if (this.hash !== "") {
+      e.preventDefault();
+      $(document).off("scroll");
+
+      $("a").each(function() {
+        $(this).removeClass("active");
+      });
+      $(this).addClass("active");
+      
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - $('.navBar').height() - $('.subNavigation').height()
+      }, 800, 'swing', function(){
+        $(document).on("scroll", onScroll);
+      });
     }
-  });  
-
-  //smooth scroll
-  $('a[href^="#"]').on("click", function(e) {
-    e.preventDefault();
-    $(document).off("scroll");
-
-    $("a").each(function() {
-      $(this).removeClass("active");
-    })
-    $(this).addClass("active");
-
-    var target = this.hash,
-        // menu = target;
-        $target = $(target);
-    $("html, body").stop().animate({
-     "scrollTop": $target.offset().top
-    }, 600, 'swing', function() {
-      window.location.hash = target;
-      $(document).on("scroll", onScroll);
-    });
   });
 });
 
-function onScroll(event) {
-  var scrollPos = $(document).scrollTop();
+
+// --------------------------------------------------
+// Toggle Subnavigation Menu Active Class on Scroll
+// --------------------------------------------------
+function onScroll() {
+  var scrollPos = $(document).scrollTop() + $('.navBar').height() + $('.subNavigation').height();
+
   $('.subNavigation a').each(function () {
     var currLink = $(this);
     var refElement = $(currLink.attr("href"));
+    
     if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
       $('.subNavigation a').removeClass("active");
       currLink.addClass("active");
@@ -89,4 +95,4 @@ function onScroll(event) {
       currLink.removeClass("active");
     }
   });
-}
+};
