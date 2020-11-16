3d9c6dad9f865e934c7b7cabacf424ff675afa06
CFMEdu <69703969+CFMEdu@users.noreply.github.com>
2020-11-16 11:03:07 -0800

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,56 +1,68 @@
 <?php
-  
-if($_POST) {
-    $visitor_name = "";
-    $visitor_email = "";
-    $email_subject = "";
-    $visitor_message = "";
-    $email_body = "<div>";
-      
-    if(isset($_POST['visitor_name'])) {
-        $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
-        $email_body .= "<div>
-                           <label><b>Visitor Name:</b></label>&nbsp;<span>".$visitor_name."</span>
-                        </div>";
-    }
- 
-    if(isset($_POST['visitor_email'])) {
-        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
-        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
-        $email_body .= "<div>
-                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$visitor_email."</span>
-                        </div>";
-    }
-      
-    if(isset($_POST['email_subject'])) {
-        $email_subject = filter_var($_POST['email_subject'], FILTER_SANITIZE_STRING);
-        $email_body .= "<div>
-                           <label><b>Reason For Contacting Us:</b></label>&nbsp;<span>".$email_subject."</span>
-                        </div>";
-    }
-      
-    if(isset($_POST['visitor_message'])) {
-        $visitor_message = htmlspecialchars($_POST['visitor_message']);
-        $email_body .= "<div>
-                           <label><b>Visitor Message:</b></label>
-                           <div>".$visitor_message."</div>
-                        </div>";
-    }
-      
-    $recipient = "cfm@cfmedu.com";
-      
-    $email_body .= "</div>";
- 
-    $headers  = 'MIME-Version: 1.0' . "\r\n"
-    .'Content-type: text/html; charset=utf-8' . "\r\n"
-    .'From: ' . $visitor_email . "\r\n";
-      
-    if(mail($recipient, $email_subject, $email_body, $headers)) {
-        echo "<p>Dear $visitor_name, thank you for getting in touch! We appreciate you contacting CFM. We will get back in touch with you soon. Have a great day!</p>";
-    } else {
-        echo '<p>We are sorry but the email did not go through.</p>';
-    }
-} else {
-    echo '<p>Something went wrong</p>';
-}
+	
+  function Redirect($url, $permanent = false) {
+      if (headers_sent() === false) {
+          header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+      }
+      exit();
+  }
+
+  if ($_SERVER['HTTP_REFERER']) {
+      $redirect_url = $_SERVER['HTTP_REFERER'];
+  } else {
+      $redirect_url = 'index.html';
+  }
+
+  if (strlen(trim($_POST['fname'])) < 1) {
+      Redirect($redirect_url);
+  } else {
+
+      $data = $_POST;
+      $to = "cfmeducational.services@gmail.com";
+    	$subject = "[CFM Website Message]";
+
+      $message = "
+        <html>
+        <head>
+          <title>CFM contact form</title>
+        </head>
+        <body>
+          <table>
+            <tr>
+              <td><strong>First Name:</strong></td>
+              <td>${data['fname']}</td>
+            </tr>
+            <tr>
+              <td><strong>Last Name:</strong></td>
+              <td>${data['lname']}</td>
+            </tr>
+            <tr>
+              <td><strong>Email:</strong></td>
+              <td>${data['email']}</td>
+            </tr>
+            <tr>
+              <td><strong>Subject:</strong></td>
+              <td>${data['subject']}</td>
+            </tr>
+            <tr>
+              <td><strong>Message:</strong></td>
+              <td>${data['message']}</td>
+            </tr>
+          </table>
+        </body>
+        </html>
+      ";
+
+      $headers  = 'MIME-Version: 1.0' . "\r\n";
+      $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+
+      $headers .= 'From: CFM Educational Services <cfm@cfmedu.com>' . "\r\n";
+
+      if (mail($to, $subject, $message, $headers)) {
+          Redirect('thank.html');
+      } else {
+          Redirect($redirect_url);
+      }
+  }
+
 ?>
